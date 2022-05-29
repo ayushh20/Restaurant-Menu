b1cc3a8e64733d80208a059c8da9d8b05d74177c
Ayush Chaurasia <ayushchaurasia.jmi@gmail.com>
2022-05-30 00:58:31 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Restaurant from "./Component/basics/Resturant";
+
+const App = () => {
+  return (
+    <div>
+      <Restaurant />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component/basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "You can’t buy happiness but you can buy MAGGI and that’s kind of the same thing….Maggi is not a food, It’s an Emotion.",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoda",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. ",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Masala corn is a favorite with kids and most of us. Spicy, nicely cooked corn with different kinds of herbs and spices. ",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "This Punjabi Chana Masala, also known as Chole Masala, is an authentic North Indian style curry made with white chickpeas, freshly powdered spices, onions, tomatoes and herbs. Naturally vegan and packed with healthy minerals, protein and fiber, this delicious vegetarian meal.",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "You can't buy happiness. But you can buy pizza, and that's kind of the same thing.Beauty comes in all shapes and sizes. Small, large, circle, square, thin crust, thick crust, stuffed crust, extra toppings ",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "A plate full spicy non-veg curry with flavour of every herb and spices.",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "We think sweet dessert is the most important meal of the day. The perfect dessert is like music that makes my taste buds dance.",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      " When you’re in the worst mood ever and suddenly you realize there’s Rajma Chawal for dinner. Bad mood? What bad mood? Imma eat my blues away!",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "Pull up a chair. Take a taste. Come join us. Life is so endlessly delicious when there is 'Chai and Samosa' ",
+  },
+];
+
+export default Menu;
